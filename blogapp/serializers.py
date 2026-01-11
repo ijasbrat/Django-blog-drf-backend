415a8e9a6dce16e58752f78461848075d7ec52f7
ijasbrat <146550669+ijasbrat@users.noreply.github.com>
2026-01-11 12:55:44 +0530

    Update serializers.py

--- blogapp/serializers.py
+++ blogapp/serializers.py
@@ -1,16 +1,11 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import Blog
-
-
-
 class UpdateUserProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
         fields = ["id", "email", "username", "first_name", "last_name", "bio", "job_title", "profile_picture",
                   "facebook", "youtube", "instagram", "twitter"]
-
-
 class UserRegistrationSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
@@ -33,7 +28,6 @@ class UserRegistrationSerializer(serializers.ModelSerializer):
         new_user.save()
         return new_user
     
-
 class SimpleAuthorSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
@@ -44,16 +38,11 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = ['id', 'title', 'slug', 'author', 'category', 'content', 'featured_image', 'published_date', 'created_at', 'updated_at', 'is_draft']
-
-
-
 class UserInfoSerializer(serializers.ModelSerializer):
     author_posts = serializers.SerializerMethodField()
     class Meta:
         model = get_user_model()
         fields = ["id", "username", "email", "first_name", "last_name", "job_title", "bio", "profile_picture", "author_posts"]
-
-    
     def get_author_posts(self, user):
         blogs = Blog.objects.filter(author=user)[:9]
         serializer = BlogSerializer(blogs, many=True)
