314f261c852c82e8af70f6320e2741fdbf6190b1
ijasbrat <146550669+ijasbrat@users.noreply.github.com>
2026-01-11 12:55:02 +0530

    Update models.py

--- blogapp/models.py
+++ blogapp/models.py
@@ -3,10 +3,6 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 from django.utils.text import slugify
 from django.utils import timezone
-
-# Create your models here.
-
-
 class CustomUser(AbstractUser):
     bio = models.TextField(blank=True, null=True)
     profile_picture = models.ImageField(upload_to="profile_img", blank=True, null=True)
@@ -18,10 +14,6 @@ class CustomUser(AbstractUser):
     instagram = models.URLField(max_length=255, blank=True, null=True)
     twitter = models.URLField(max_length=255, blank=True, null=True)
     linkedin = models.URLField(max_length=255, blank=True, null=True)
-
-
-
-
     def __str__(self):
         return self.username
     
